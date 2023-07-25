875ceef24df5ec3eaedf0b329edba8b1017f6335
Razan Hallak <razanhalla@gmail.com>
2023-07-25 13:10:46 +0300

    15

--- Journal/Journal/settings.py
+++ Journal/Journal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool) 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"] # ".vercel.app"
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*.vercel.app", "*Journal.vercel.app" ,"*"] # ".vercel.app"
 
 
 # Application definition
